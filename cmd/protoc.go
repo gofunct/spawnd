b859915ea3bb4ca60e6fb40972eed0178d362e32
Coleman Word <coleman@Colemans-MacBook-Pro.local>
2018-12-19 00:45:07 -0700

    init

---
+++ cmd/protoc.go
@@ -0,0 +1,148 @@
+package cmd // import "moul.io/protoc-gen-gotemplate"
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/generator"
+	"github.com/golang/protobuf/protoc-gen-go/plugin"
+	ggdescriptor "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway/descriptor"
+
+	pgghelpers "moul.io/protoc-gen-gotemplate/helpers"
+)
+
+var (
+	registry *ggdescriptor.Registry // some helpers need access to registry
+)
+
+const (
+	boolTrue  = "true"
+	boolFalse = "false"
+)
+
+func main() {
+	g := generator.New()
+
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		g.Error(err, "reading input")
+	}
+
+	if err = proto.Unmarshal(data, g.Request); err != nil {
+		g.Error(err, "parsing input proto")
+	}
+
+	if len(g.Request.FileToGenerate) == 0 {
+		g.Fail("no files to generate")
+	}
+
+	g.CommandLineParameters(g.Request.GetParameter())
+
+	// Parse parameters
+	var (
+		templateDir       = "./templates"
+		destinationDir    = "."
+		debug             = false
+		all               = false
+		singlePackageMode = false
+	)
+	if parameter := g.Request.GetParameter(); parameter != "" {
+		for _, param := range strings.Split(parameter, ",") {
+			parts := strings.Split(param, "=")
+			if len(parts) != 2 {
+				log.Printf("Err: invalid parameter: %q", param)
+				continue
+			}
+			switch parts[0] {
+			case "template_dir":
+				templateDir = parts[1]
+			case "destination_dir":
+				destinationDir = parts[1]
+			case "single-package-mode":
+				switch strings.ToLower(parts[1]) {
+				case boolTrue, "t":
+					singlePackageMode = true
+				case boolFalse, "f":
+				default:
+					log.Printf("Err: invalid value for single-package-mode: %q", parts[1])
+				}
+			case "debug":
+				switch strings.ToLower(parts[1]) {
+				case boolTrue, "t":
+					debug = true
+				case boolFalse, "f":
+				default:
+					log.Printf("Err: invalid value for debug: %q", parts[1])
+				}
+			case "all":
+				switch strings.ToLower(parts[1]) {
+				case boolTrue, "t":
+					all = true
+				case boolFalse, "f":
+				default:
+					log.Printf("Err: invalid value for debug: %q", parts[1])
+				}
+			default:
+				log.Printf("Err: unknown parameter: %q", param)
+			}
+		}
+	}
+
+	tmplMap := make(map[string]*plugin_go.CodeGeneratorResponse_File)
+	concatOrAppend := func(file *plugin_go.CodeGeneratorResponse_File) {
+		if val, ok := tmplMap[file.GetName()]; ok {
+			*val.Content += file.GetContent()
+		} else {
+			tmplMap[file.GetName()] = file
+			g.Response.File = append(g.Response.File, file)
+		}
+	}
+
+	if singlePackageMode {
+		registry = ggdescriptor.NewRegistry()
+		pgghelpers.SetRegistry(registry)
+		if err = registry.Load(g.Request); err != nil {
+			g.Error(err, "registry: failed to load the request")
+		}
+	}
+
+	// Generate the encoders
+	for _, file := range g.Request.GetProtoFile() {
+		if all {
+			if singlePackageMode {
+				if _, err = registry.LookupFile(file.GetName()); err != nil {
+					g.Error(err, "registry: failed to lookup file %q", file.GetName())
+				}
+			}
+			encoder := NewGenericTemplateBasedEncoder(templateDir, file, debug, destinationDir)
+			for _, tmpl := range encoder.Files() {
+				concatOrAppend(tmpl)
+			}
+
+			continue
+		}
+
+		for _, service := range file.GetService() {
+			encoder := NewGenericServiceTemplateBasedEncoder(templateDir, service, file, debug, destinationDir)
+			for _, tmpl := range encoder.Files() {
+				concatOrAppend(tmpl)
+			}
+		}
+	}
+
+	// Generate the protobufs
+	g.GenerateAllFiles()
+
+	data, err = proto.Marshal(g.Response)
+	if err != nil {
+		g.Error(err, "failed to marshal output proto")
+	}
+
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		g.Error(err, "failed to write output proto")
+	}
+}
--- go.mod
+++ go.mod
@@ -3,26 +3,27 @@ module github.com/gofunct/grpcgen
 require (
 	cloud.google.com/go v0.34.0 // indirect
 	github.com/Masterminds/semver v1.4.2 // indirect
-	github.com/Masterminds/sprig v2.16.0+incompatible // indirect
+	github.com/Masterminds/sprig v2.16.0+incompatible
 	github.com/abronan/todo-grpc v0.0.0-20180410102811-34af5e331818 // indirect
 	github.com/aokoli/goutils v1.0.1 // indirect
 	github.com/ckaznocha/protoc-gen-lint v0.2.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/fatih/color v1.7.0
 	github.com/ghodss/yaml v1.0.0 // indirect
-	github.com/go-kit/kit v0.8.0
+	github.com/go-kit/kit v0.8.0 // indirect
 	github.com/go-logfmt/logfmt v0.4.0 // indirect
 	github.com/go-pg/pg v6.15.1+incompatible // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gofunct/rpctemplates v0.0.0-20181219062029-2b831dcd1490 // indirect
 	github.com/gogo/protobuf v1.2.0 // indirect
 	github.com/golang/mock v1.2.0 // indirect
+	github.com/golang/protobuf v1.2.0
 	github.com/google/uuid v1.1.0 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/handlers v1.4.0
-	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/mux v1.6.2
 	github.com/grpc-ecosystem/grpc-gateway v1.6.2
-	github.com/huandu/xstrings v1.2.0 // indirect
+	github.com/huandu/xstrings v1.2.0
 	github.com/iancoleman/strcase v0.0.0-20180726023541-3605ed457bf7 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -37,18 +38,18 @@ require (
 	github.com/sirupsen/logrus v1.2.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3
-	github.com/spf13/viper v1.3.1
+	github.com/spf13/viper v1.3.1 // indirect
 	github.com/xlab/treeprint v0.0.0-20181112141820-a009c3971eca // indirect
 	golang.org/x/lint v0.0.0-20181217174547-8f45f776aaf1 // indirect
-	golang.org/x/net v0.0.0-20181217023233-e147a9138326
+	golang.org/x/net v0.0.0-20181217023233-e147a9138326 // indirect
 	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890 // indirect
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
 	golang.org/x/sys v0.0.0-20181217223516-dcdaa6325bcb // indirect
 	golang.org/x/tools v0.0.0-20181218020041-13ba8ad772df // indirect
 	google.golang.org/appengine v1.3.0 // indirect
-	google.golang.org/genproto v0.0.0-20181217233458-174793a81d5f // indirect
-	google.golang.org/grpc v1.17.0
+	google.golang.org/genproto v0.0.0-20181217233458-174793a81d5f
+	google.golang.org/grpc v1.17.0 // indirect
 	honnef.co/go/tools v0.0.0-20180920025451-e3ad64cb4ed3 // indirect
 	mellium.im/sasl v0.2.1 // indirect
-	moul.io/protoc-gen-gotemplate v1.6.0 // indirect
+	moul.io/protoc-gen-gotemplate v1.6.0
 )
